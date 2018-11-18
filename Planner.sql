c7a63c7b0e1d1761dcef570512febd2280d6234e
jpak2018 <38224854+jpak2018@users.noreply.github.com>
2018-11-18 13:01:41 -0800

    Change variable type
    mySQL doesn't accept NUM Like oracle SQL so had to switch them into INTS

--- Planner.sql
+++ Planner.sql
@@ -6,14 +6,14 @@ SELECT 'CREATING DATABASE STRUCTURE' as 'INFO';
 
 DROP TABLE IF EXISTS Events, User, Goingto, Friends, Review;
               
-CREATE TABLE Events (eventid num(10), ename varchar(20), location varchar(20), eventdate date, starttime time, endtime time  PRIMARY KEY(eventid));
-CREATE TABLE User (uemail num(10), fname varchar(20), lname varchar(20), PRIMARY KEY(uemail));
-CREATE TABLE Goingto (eventid num(10), userid num(10), goingdate date, PRIMARY KEY(eventid,userid),
+CREATE TABLE Events (eventid INT, ename varchar(20), location varchar(20), eventdate date, starttime time, endtime time  PRIMARY KEY(eventid));
+CREATE TABLE User (uemail INT, fname varchar(20), lname varchar(20), PRIMARY KEY(uemail));
+CREATE TABLE Goingto (eventid INT, userid INT, goingdate date, PRIMARY KEY(eventid,userid),
 FOREIGN KEY (eventid) REFERENCES Events (eventid),
 FOREIGN KEY (userid) REFERENCES User (userid) );
-CREATE TABLE Friends (userid num(10),friendid num(10),
+CREATE TABLE Friends (userid INT,friendid num(10),
 PRIMARY KEY (userid,friendid),
 FOREIGN KEY (userid) REFERENCES User (userid),
 FOREIGN KEY (friendid) REFERENCES User (userid));
-CREATE TABLE Review (review varchar(500),ename varchar(20), attenddate date, rating num(10),PRIMARY KEY(ename,attenddate),
+CREATE TABLE Review (review varchar(500),ename varchar(20), attenddate date, rating INT,PRIMARY KEY(ename,attenddate),
 FOREIGN KEY (ename) REFERENCES Events (ename));
