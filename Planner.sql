e666182b0c6b17d76dcebae8e61af2ed774dfe0d
jpak2018 <38224854+jpak2018@users.noreply.github.com>
2018-11-18 12:46:25 -0800

    Adding Create Database

--- Planner.sql
+++ Planner.sql
@@ -1,3 +1,11 @@
+DROP DATABASE IF EXISTS planner;
+CREATE DATABASE IF NOT EXISTS planner;
+USE planner;
+
+SELECT 'CREATING DATABASE STRUCTURE' as 'INFO';
+
+DROP TABLE IF EXISTS Events, User, Goingto, Friends, Review;
+              
 CREATE TABLE Events (eventid num(10), ename varchar(20), location varchar(20), eventdate date, starttime time, endtime time  PRIMARY KEY(eventid));
 CREATE TABLE User (uemail num(10), fname varchar(20), lname varchar(20), PRIMARY KEY(uemail));
 CREATE TABLE Goingto (eventid num(10), userid num(10), goingdate date, PRIMARY KEY(eventid,userid),
