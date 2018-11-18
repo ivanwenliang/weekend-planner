272d87f9e23d177d6dc1eaf684db6674ff439760
jpak2018 <38224854+jpak2018@users.noreply.github.com>
2018-11-18 12:33:26 -0800

    Update Planner.sql
    Changes made to the table to account for the different variables we will be using

--- Planner.sql
+++ Planner.sql
@@ -1,5 +1,5 @@
-CREATE TABLE Events (eventid num(10), category varchar(20), tstamp date, ename varchar(20), PRIMARY KEY(eventid));
-CREATE TABLE User (userid num(10), fname varchar(20), lname varchar(20), PRIMARY KEY(userid));
+CREATE TABLE Events (eventid num(10), ename varchar(20), location varchar(20), eventdate date, starttime time, endtime time  PRIMARY KEY(eventid));
+CREATE TABLE User (uemail num(10), fname varchar(20), lname varchar(20), PRIMARY KEY(uemail));
 CREATE TABLE Goingto (eventid num(10), userid num(10), goingdate date, PRIMARY KEY(eventid,userid),
 FOREIGN KEY (eventid) REFERENCES Events (eventid),
 FOREIGN KEY (userid) REFERENCES User (userid) );
@@ -7,3 +7,5 @@ CREATE TABLE Friends (userid num(10),friendid num(10),
 PRIMARY KEY (userid,friendid),
 FOREIGN KEY (userid) REFERENCES User (userid),
 FOREIGN KEY (friendid) REFERENCES User (userid));
+CREATE TABLE Review (review varchar(500),ename varchar(20), attenddate date, rating num(10),PRIMARY KEY(ename,attenddate),
+FOREIGN KEY (ename) REFERENCES Events (ename));
