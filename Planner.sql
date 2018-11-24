62ce51eb8e8f3e54b98f1c183197fb96fb70a238
Ivan Liang <ivanwenliang@gmail.com>
2018-11-24 13:34:09 -0800

    Changes to planner database.

--- Planner.sql
+++ Planner.sql
@@ -7,13 +7,13 @@ SELECT 'CREATING DATABASE STRUCTURE' as 'INFO';
 DROP TABLE IF EXISTS Events, User, Goingto, Friends, Review;
               
 CREATE TABLE Events (eventid INT, ename varchar(100), location varchar(100), eventdate varchar(50), starttime varchar(20), PRIMARY KEY(eventid));
-CREATE TABLE User (uemail INT, fname varchar(20), lname varchar(20), PRIMARY KEY(uemail));
-CREATE TABLE Goingto (eventid INT, userid INT, goingdate date, PRIMARY KEY(eventid,userid),
+CREATE TABLE User (uemail varchar(100), fname varchar(20), lname varchar(20), PRIMARY KEY(uemail));
+CREATE TABLE Goingto (eventid INT, uemail varchar(100), goingdate date, PRIMARY KEY(eventid,uemail),
 FOREIGN KEY (eventid) REFERENCES Events (eventid),
-FOREIGN KEY (userid) REFERENCES User (userid) );
-CREATE TABLE Friends (userid INT,friendid INT(10),
-PRIMARY KEY (userid,friendid),
-FOREIGN KEY (userid) REFERENCES User (userid),
-FOREIGN KEY (friendid) REFERENCES User (userid));
-CREATE TABLE Review (review varchar(500),ename varchar(20), attenddate date, rating INT,PRIMARY KEY(ename,attenddate),
-FOREIGN KEY (ename) REFERENCES Events (ename));
+FOREIGN KEY (uemail) REFERENCES User (uemail) );
+-- CREATE TABLE Friends (userid INT,friendid INT(10),
+-- PRIMARY KEY (userid,friendid),
+-- FOREIGN KEY (userid) REFERENCES User (userid),
+-- FOREIGN KEY (friendid) REFERENCES User (userid));
+-- CREATE TABLE Review (review varchar(500),ename varchar(100), attenddate date, rating INT,PRIMARY KEY(ename,attenddate),
+-- FOREIGN KEY (ename) REFERENCES Events (ename));
