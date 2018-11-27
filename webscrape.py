f74e5e60aa32cb91272f5c7ded4eb9c99fa68d23
Dan <d.mazza830@gmail.com>
2018-11-27 12:42:07 -0800

    added description link

--- webscrape.py
+++ webscrape.py
@@ -2,94 +2,91 @@ from urllib.request import urlopen as uReq
 from bs4 import BeautifulSoup as soup
 import mysql.connector
 
-#global variable to keep track of max event id
-event_id_counter = 1
+#region
+#global variables
+
+event_tag_classes = {'event_name' : {"class": "card-text--truncated__three"},
+                     'event_month' : {"class": "date-thumbnail__month"},
+                     'event_day' : {"class": "date-thumbnail__day"},
+                     'event_location' : {"class": "card-text--truncated__one"},
+                     'event_time' : {"class": "eds-media-card-content__sub-content"},
+                     'event_desc' : {"class": "eds-media-card-content__action-link"}
+                     }
+
+event_info_dict = {}
+#endregion
 
 def main():
 
     mydb = mysql.connector.connect(host='127.0.0.1', user='root', password='gosantaclara', database='planner')
     mydb.autocommit = True
 
-    url_queue = ['https://www.eventbrite.com/d/ca--san-jose/events/']
-    html_tag_queue = []
-    event_info_dict = {}
+    url ='https://www.eventbrite.com/d/ca--san-jose/events/'
 
-    populateHtmlTagQueue(html_tag_queue)
-    scrapeToTextFile(url_queue, html_tag_queue, event_info_dict,mydb)
+    scrapeEventList(url,mydb)
 
     mydb.close()
 
-def populateHtmlTagQueue(html_tag_queue):
-	html_tag_queue.append({"class": "eds-media-card-content__content"})
-	html_tag_queue.append({"class": "card-text--truncated__three"})
-	html_tag_queue.append({"class": "date-thumbnail__month"})
-	html_tag_queue.append({"class": "date-thumbnail__day"})
-	html_tag_queue.append({"class": "card-text--truncated__one"})
-	html_tag_queue.append({"class": "eds-media-card-content__sub-content"})
-
-def scrapeToTextFile(url_queue, html_tag_queue, event_info_dict,database):
- 
- for url in url_queue:
+def scrapeEventList(url,database):
 
 	#opens a conenction and grabs the webpage
 	uClient = uReq(url)
 	page_html = uClient.read()
 	uClient.close()
 
-	event_list = []
-
 	page_soup = soup(page_html, "html.parser")
 
-	containers = page_soup.findAll("div", html_tag_queue[0])
-	html_tag_queue.pop(0)
+	containers = page_soup.findAll("div", {"class": "eds-media-card-content__content"})
+
+	event_id_counter = 1
 
 	for container in containers:
 
-		name_container = container.findAll("div", html_tag_queue[0])
-		if len(name_container) != 0:
-			#check for duplicates here to not waste time
-		    if name_container[0].text not in event_list:
-		        event_info_dict["event_name"] = name_container[0].text
-		        event_list.append(event_info_dict["event_name"])
-		    else:
-		    	#if event has already been parsed from another website, go to next iteration of the loop
-		    	continue
+		event_info_dict['event_name'] = pullTextFromTag(container, 'div', event_tag_classes['event_name'])
 
-		month_container = container.findAll("p", html_tag_queue[1])
-		if len(month_container) != 0:
-		    event_month = month_container[0].text
+		#pull event month and day and concatenate them
+		event_month = pullTextFromTag(container, 'p', event_tag_classes['event_month'])
+		event_day = pullTextFromTag(container, 'p', event_tag_classes['event_day'])
+		if event_month != None and event_day != None:
+		    event_info_dict["event_date"] = str(event_month + " " + event_day)
 
-		day_container = container.findAll("p", html_tag_queue[2])
-		if len(day_container) != 0:
-			event_day = day_container[0].text
+		event_info_dict["event_location"] = pullTextFromTag(container, 'div', event_tag_classes['event_location'])
 
-		event_info_dict["event_date"] = str(event_month + " " + event_day)
-
-		location_conatiner = container.findAll("div", html_tag_queue[3])
-		if len(location_conatiner) != 0:
-		    event_info_dict["event_location"] = location_conatiner[0].text
-
-		time_container = container.findAll("div",html_tag_queue[4])
+        #time has to be handled differently because of the way it is formatted on the site
+		time_container = container.findAll("div",event_tag_classes['event_time'])
 		if len(time_container) != 0:
 		    event_time = time_container[0].div.text.split(',')
 		    if len(event_time) >= 2:
 		        event_time = event_time[2]
 		        event_info_dict["event_time"] = event_time[0:8]
 
+		event_info_dict['event_desc'] = scrapeEventDesc(container) 
+
 		event_info_dict["event_id"] = event_id_counter
-		event_id_counter = event_id_counter + 1
+		event_id_counter += 1
 
 		populateEventDb(event_info_dict,database)
 		event_info_dict.clear
-	html_tag_queue.pop[0:5]
 
+def scrapeEventDesc(container):
+	event_desc_link_container = container.findAll("a", event_tag_classes['event_desc'])
+	for a in event_desc_link_container:
+		event_desc_link = a['href']
+
+	return event_desc_link
+
+def pullTextFromTag(container,html_tag,html_class):
+	text_container = container.findAll(html_tag, html_class)
+	if len(text_container) != 0:
+	    return text_container[0].text
+	else:
+		return 
 
 def populateEventDb(event_info_dict,database):
-	
 	mycursor = database.cursor()
-	sqlFormula = "INSERT INTO events (ename,location,eventdate,starttime) VALUES (%s,%s,%s,%s,%s)"
+	sqlFormula = "INSERT INTO events (ename,location,eventdate,starttime) VALUES (%s,%s,%s,%s,%s,%s)"
 	event_info = (event_info_dict["event_id"],event_info_dict["event_name"],event_info_dict["event_location"],
-		event_info_dict["event_date"],event_info_dict["event_time"])
+		event_info_dict["event_date"],event_info_dict["event_time"],event_info_dict['event_desc'])
 
 	mycursor.execute(sqlFormula,event_info)
 
