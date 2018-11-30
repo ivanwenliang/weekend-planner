080e82d51aa2ed2886ae4b3da5ea07db58e44b1a
jpak2018 <38224854+jpak2018@users.noreply.github.com>
2018-11-29 21:18:33 -0800

    Updated Goingto table

--- Planner.sql
+++ Planner.sql
@@ -8,9 +8,9 @@ DROP TABLE IF EXISTS Events, User, Goingto, Friends, Review;
               
 CREATE TABLE Events (eventid INT, ename varchar(100), location varchar(100), eventdate varchar(50), starttime varchar(20), edesc varchar(300), PRIMARY KEY(eventid));
 CREATE TABLE User (id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, username varchar(50) NOT NULL UNIQUE, password varchar(255) NOT NULL, firstname varchar(100) NOT NULL, lastname varchar(100));
--- CREATE TABLE Goingto (eventid INT, uemail varchar(100), goingdate date, PRIMARY KEY(eventid,uemail),
--- FOREIGN KEY (eventid) REFERENCES Events (eventid),
--- FOREIGN KEY (uemail) REFERENCES User (uemail) );
+CREATE TABLE Goingto (eventid INT, id INT, PRIMARY KEY(eventid,id),
+FOREIGN KEY fk_eventid (eventid) REFERENCES Events (eventid),
+FOREIGN KEY fk_userid (id) REFERENCES User (id) );
 -- CREATE TABLE Friends (userid INT,friendid INT(10),
 -- PRIMARY KEY (userid,friendid),
 -- FOREIGN KEY (userid) REFERENCES User (userid),
