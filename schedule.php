58ec71be638f8b741c6ed7a7365c59031e326913
Ivan Liang <ivanwenliang@gmail.com>
2018-11-29 23:24:03 -0800

    Add review page to nav

--- schedule.php
+++ schedule.php
@@ -42,6 +42,9 @@
 						<li class="nav-item">
 							<a class="nav-link" href="schedule.php">Schedule</a>
 						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="Reviewinputform.html">Reviews</a>
+						</li>
 					</ul>	
 					<ul class="navbar-nav">
 						<li class="nav-item">
