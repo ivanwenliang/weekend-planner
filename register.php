280c15543470c1d566671fe84728dc8e0744645e
jpak2018 <38224854+jpak2018@users.noreply.github.com>
2018-11-29 17:38:55 -0800

    Change Username Part

--- register.php
+++ register.php
@@ -1,58 +1,44 @@
 <?php
 // Include config file
-
 $servername = "127.0.0.1";
-
 $username = "root";
-
 $password = "IbgrwAttn,mwa.11SQL";
-
 $dbname = "planner";
-
 // Create connection
-
 $link = new mysqli($servername, $username, $password, $dbname);
-
 // Define variables and initialize with empty values
 $username = $password = $confirm_password = "";
 $username_err = $password_err = $confirm_password_err = "";
-
 // Processing form data when form is submitted
 if($_SERVER["REQUEST_METHOD"] == "POST"){
-
     // Validate username
     if(empty(trim($_POST["username"]))){
         $username_err = "Please enter a username.";
     } else{
-        // Prepare a select statement
-        $sql = "SELECT id FROM users WHERE username = ?";
-
-        if($stmt = mysqli_prepare($link, $sql)){
-            // Bind variables to the prepared statement as parameters
-            mysqli_stmt_bind_param($stmt, "s", $param_username);
-
-            // Set parameters
-            $param_username = trim($_POST["username"]);
-
-            // Attempt to execute the prepared statement
-            if(mysqli_stmt_execute($stmt)){
-                /* store result */
-                mysqli_stmt_store_result($stmt);
-
-                if(mysqli_stmt_num_rows($stmt) == 1){
-                    $username_err = "This username is already taken.";
-                } else{
-                    $username = trim($_POST["username"]);
-                }
-            } else{
-                echo "Oops! Something went wrong. Please try again later.";
-            }
-        }
-
-        // Close statement
-        mysqli_stmt_close($stmt);
+        $username = trim($_POST["username"])
+        // // Prepare a select statement
+        // $sql = "SELECT id FROM users WHERE username = ?";
+        // if($stmt = mysqli_prepare($link, $sql)){
+        //     // Bind variables to the prepared statement as parameters
+        //     mysqli_stmt_bind_param($stmt, "s", $param_username);
+        //     // Set parameters
+        //     $param_username = trim($_POST["username"]);
+        //     // Attempt to execute the prepared statement
+        //     if(mysqli_stmt_execute($stmt)){
+        //         /* store result */
+        //         mysqli_stmt_store_result($stmt);
+        //         if(mysqli_stmt_num_rows($stmt) == 1){
+        //             $username_err = "This username is already taken.";
+        //         } else{
+        //             $username = trim($_POST["username"]);
+        //         }
+        //     } else{
+        //         echo "Oops! Something went wrong. Please try again later.";
+        //     }
+        // }
+        // // Close statement
+        // mysqli_stmt_close($stmt);
     }
-
     // Validate password
     if(empty(trim($_POST["password"]))){
         $password_err = "Please enter a password.";
@@ -61,7 +47,6 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     } else{
         $password = trim($_POST["password"]);
     }
-
     // Validate confirm password
     if(empty(trim($_POST["confirm_password"]))){
         $confirm_password_err = "Please confirm password.";
@@ -71,21 +56,16 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
             $confirm_password_err = "Password did not match.";
         }
     }
-
     // Check input errors before inserting in database
     if(empty($username_err) && empty($password_err) && empty($confirm_password_err)){
-
         // Prepare an insert statement
-        $sql = "INSERT INTO User (username, password) VALUES ('$param_username', '$password')";
-
+        $sql = "INSERT INTO User (username, password) VALUES ('$username', '$password')";
         if($stmt = mysqli_prepare($link, $sql)){
             // Bind variables to the prepared statement as parameters
             mysqli_stmt_bind_param($stmt, "ss", $param_username, $param_password);
-
             // Set parameters
             $param_username = $username;
             $param_password = password_hash($password, PASSWORD_DEFAULT); // Creates a password hash
-
             // Attempt to execute the prepared statement
             if(mysqli_stmt_execute($stmt)){
                 // Redirect to login page
@@ -94,11 +74,9 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
                 echo "Something went wrong. Please try again later.";
             }
         }
-
         // Close statement
         mysqli_stmt_close($stmt);
     }
-
     // Close connection
     mysqli_close($link);
 }
